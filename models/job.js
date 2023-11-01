06188d93077b24d9fd2fe80dc17de073dc32945c
Debarun <paldebarun27@gmail.com>
2023-11-01 23:27:58 +0530

    changes are made

--- models/job.js
+++ models/job.js
@@ -25,33 +25,7 @@ const Job=new mongoose.Schema({
 });
 
 
-Job.post("save",async function(doc){
-  try{
-       
-      console.log("the document is ",doc);
-  
-      let transporter = nodemailer.createTransport({
-          host: process.env.MAIL_HOST,
-          auth:{
-              user:process.env.MAIL_USER,
-              pass: process.env.MAIL_PASS,
-          },
-      });
-  
-      let info = await transporter.sendMail({
-          from:`debarun`,
-          to: doc.email,
-          subject: "your job is uploaded on the site",
-          html:`<h2>Hello Jee</h2> <p>product Uploaded</p>`,
-      })
-      
-      console.log("INFO", info);
-  
-  }
-  catch(error){
-    console.log("mail can't be sent");
-  }
-  });
+
 
 
 
